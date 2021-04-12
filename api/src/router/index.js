1d882f5215d4c176e65d222743ab610a9f2390f8
YoussefBA-codings <72443104+YoussefBA-codings@users.noreply.github.com>
2021-04-12 12:18:40 +0200

    Update index.js

--- api/src/router/index.js
+++ api/src/router/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
-const ImmobilierRoutes = require('./routes/Immobilier.routes')
-const LoginRoutes = require('./routes/Login.routes')
-const UserRoutes = require('./routes/User.routes')
-
+const ImmobilierRoutes = require('./routes/Immobilier.routes');
+const LoginRoutes = require('./routes/Login.routes');
+const UserRoutes = require('./routes/User.routes');
+const AppUserRoutes = require('./routes/AppUser.route');
 
 const router = express.Router();
 
-ImmobilierRoutes(router)
-LoginRoutes(router)
-UserRoutes(router)
+ImmobilierRoutes(router);
+LoginRoutes(router);
+UserRoutes(router);
+AppUserRoutes(router);
 
-module.exports = router
+module.exports = router;
