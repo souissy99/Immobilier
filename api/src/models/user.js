422cdeaad8adad1ab5e040bd6b381a76384ebd78
YoussefBA-codings <72443104+YoussefBA-codings@users.noreply.github.com>
2021-04-16 11:50:02 +0200

    Update user.js

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -1,36 +1,34 @@
 'use strict';
 const bcrypt = require('bcrypt');
 const {
-  Model
+	Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
+	class User extends Model {
+		/**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      User.hasMany(models.userSimulation, {
-				onDelete: 'cascade'
-			});
-    }
-  };
-  User.init({
-    firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      set(value) {
-        const hash = bcrypt.hashSync(value, 10);
-        this.setDataValue('password', hash);
-      }
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+		static associate (models) {
+			User.belongsTo(models.userSimulation);
+		}
+	}
+	User.init({
+		firstname: DataTypes.STRING,
+		lastname: DataTypes.STRING,
+		email: DataTypes.STRING,
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			set (value) {
+				const hash = bcrypt.hashSync(value, 10);
+				this.setDataValue('password', hash);
+			}
+		}
+	}, {
+		sequelize,
+		modelName: 'User'
+	});
+	return User;
 };
